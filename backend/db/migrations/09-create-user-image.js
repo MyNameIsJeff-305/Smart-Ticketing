d316014f7baf2b9cc173958803074b2c752af78a
Michel Garcia Ribalta <149029907+MyNameIsJeff-305@users.noreply.github.com>
2024-07-25 19:15:13 -0400

    Migrations Done

---
+++ backend/db/migrations/09-create-user-image.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+
+let options = {};
+options.tableName = 'Locations';
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('UserImages', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      url: {
+        allowNull: false,
+        type: Sequelize.STRING(50)
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal("CURRENT_TIMESTAMP")
+      }
+    }, options);
+  },
+  async down(queryInterface, Sequelize) {
+    options.tableName = 'UserImages'
+    await queryInterface.dropTable(options);
+  }
+};
