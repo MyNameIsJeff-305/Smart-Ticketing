99f65f546499192d08c010bf803e5a6997e0e7d0
Michel Garcia Ribalta <149029907+MyNameIsJeff-305@users.noreply.github.com>
2024-07-28 22:40:07 -0400

    Fixes v.23

--- backend/db/migrations/09-create-role.js
+++ backend/db/migrations/09-create-role.js
@@ -18,7 +18,7 @@ module.exports = {
       },
       name: {
         allowNull: false,
-        type: Sequelize.STRING(50)
+        type: Sequelize.STRING
       },
       description: {
         type: Sequelize.STRING
