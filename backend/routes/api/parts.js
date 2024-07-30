3e7ca2038b45fd2793b1cc5d628e124622c6e67b
Michel <michel@Michels-MBP.localdomain>
2024-07-30 16:52:06 -0400

    Commit for switch between stations

---
+++ backend/routes/api/parts.js
@@ -0,0 +1,21 @@
+const express = require('express');
+
+const { requireAuth } = require('../../utils/auth');
+
+const router = express.Router();
+
+//Routes
+//Get All Parts
+
+//Get Part Details based on Part ID
+
+//Add a PartImage to a Part based on Part ID
+
+//Create a New Part
+
+//Edit a Part based on Part ID
+
+//Delete a Part based on Part ID
+
+
+module.exports = router;
