bee10a7e22b7a0a6f280e9c2fb961d65f6811277
Michel Garcia Ribalta <149029907+MyNameIsJeff-305@users.noreply.github.com>
2024-07-31 21:25:13 -0400

    session state management

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import './index.css';
 import { Provider } from 'react-redux';
 import configureStore from './store';
 import { restoreCSRF, csrfFetch } from './store/csrf';
-// import * as sessionActions from './store/session';
+import * as sessionActions from './store/session';
 
 const store = configureStore();
 
@@ -14,7 +14,8 @@ if (import.meta.env.MODE !== 'production') {
 
   window.csrfFetch = csrfFetch;
   window.store = store;
-  // window.sessionActions = sessionActions;
+  window.sessionActions = sessionActions;
+  
 }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,13 +1,13 @@
 import { createStore, applyMiddleware, compose, combineReducers } from 'redux';
 import { thunk } from 'redux-thunk';
 
-// import sessionReducer from './session';
+import sessionReducer from './session';
 // import spotsReducer from './spots';
 // import reviewsReducer from './reviews';
 // import spotImagesReducer from './spot-images';
 
 const rootReducer = combineReducers({
-  // session: sessionReducer,
+  session: sessionReducer,
   // spots: spotsReducer,
   // reviews: reviewsReducer,
   // spotImages: spotImagesReducer
