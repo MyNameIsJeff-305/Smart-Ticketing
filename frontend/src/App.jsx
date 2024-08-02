2a287ce1be6ccd2ad0168d7df6fe56bef5c65f19
Michel Garcia Ribalta <149029907+MyNameIsJeff-305@users.noreply.github.com>
2024-08-01 23:22:50 -0400

    Login Done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,47 @@
+import { useDispatch } from "react-redux";
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+
 import LoginFormPage from "./components/LoginFormPage";
-import { ThemeProvider } from "@/components/theme-provider"
+import Dashboard from "./components/Dashboard";
+
+import { useEffect, useState } from "react";
+
+function Layout() {
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser())
+      .then(() => setIsLoaded(true));
+  }, [dispatch]);
+
+  return (
+    <>
+      <div className='main-zone'>
+        {isLoaded && <Outlet />}
+      </div>
+    </>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <LoginFormPage />
+      },
+      {
+        path: '/dashboard',
+        element: <Dashboard />
+      }
+    ]
+  }
+])
 
 function App() {
-  return (
-    <ThemeProvider>
-      <LoginFormPage />
-    </ThemeProvider>
-)
+  return <RouterProvider router={router} />
 }
 
 export default App;
--- frontend/src/components/LoginFormPage/LoginFormPage.jsx
+++ frontend/src/components/LoginFormPage/LoginFormPage.jsx
@@ -1,30 +1,86 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import * as sessionActions from '../../store/session';
+import { useDispatch } from 'react-redux';
+
 import './LoginFormPage.css';
 
 function LoginFormPage() {
-    <main>
-        <section>
-            <div>
-                <img src="frontend/dist/assets/logo.webp" alt="logo" />
-                <aside>SMART Ticketing</aside>
-            </div>
-            <aside>
-            This is a Ticket Management System for IT Businesses that handle on Site installations and Troubleshooting
-            </aside>
-        </section>
-        <section>
-            <form>
-                <label>
-                    Username or Email
-                    <input type="text" name="credential" />
-                </label>
-                <label>
-                    Password
-                    <input type="password" name="password" />
-                </label>
-                <button type="submit">Log In</button>
-            </form>
-        </section>
-    </main>
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const [credential, setCredential] = useState('');
+    const [password, setPassword] = useState('');
+    const [errors, setErrors] = useState({});
+    const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+
+    //Reset the state when the component is rendered
+    useEffect(() => {
+        setCredential('');
+        setPassword('');
+        setErrors({});
+        setIsButtonDisabled(true);
+    }, []);
+
+    useEffect(() => {
+        const newErrors = {};
+        if (credential.length > 0 && credential.length < 4) {
+            newErrors.credential = 'Username or Email must be 4 characters or longer';
+        }
+        if (password.length > 0 && password.length < 6) {
+            newErrors.password = 'Password must be 6 characters or longer';
+        }
+        setErrors(newErrors);
+        setIsButtonDisabled(credential.length < 4 || password.length < 6);
+    }, [credential, password]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setErrors({});
+
+        dispatch(sessionActions.login({ credential, password }))
+            .then(() => navigate('/dashboard'))
+            .catch(async (res) => {
+                const data = await res.json();
+                if (data && data.errors) {
+                    setErrors(data.errors);
+                }
+            });
+
+
+    }
+
+    return (
+        <main>
+            <section className='left-section'>
+                <div className="logo">
+                    <img src="../../../dist/assets/logo.png" alt="logo" />
+                </div>
+                <aside className="left-text">
+                    <aside>SMART Ticketing</aside>
+                    This is a Ticket Management System for IT Businesses that handle on Site installations and Troubleshooting
+                </aside>
+            </section>
+            <section className='right-section'>
+                <div className='form-container'>
+                    <form className="login-form">
+                        <label>
+                            Username or Email
+                            <input type="text" name="credential" value={credential} onChange={(e) => setCredential(e.target.value)} />
+                        </label>
+                        <label>
+                            Password
+                            <input type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                        </label>
+                        <button type="submit" onClick={(e) => handleSubmit(e)}>Log In</button>
+                    </form>
+                    {errors.message && (
+                        <p className='error-message'>{errors.message}</p>
+                    )}
+                </div>
+            </section>
+        </main>
+    );
 }
 
 export default LoginFormPage;
